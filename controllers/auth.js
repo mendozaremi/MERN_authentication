7dcd88d732f1f1b141482127e343fbb284e01e13
mendozaremi <mendozaremi@gmail.com>
2021-11-11 10:52:21 -0500

    password hashing

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,9 +24,34 @@ exports.register = async (req, res, next) => {
 
 }
 
-exports.login = (req, res, next) => {
-  res.send("Login Route");
-}
+exports.login = async (req, res, next) => {
+  const {email, password } = req.body;
+
+  if(!email || !password) {
+    res.status(400).json({ success: false, error: "Please provide email and pasword"})
+  }
+
+  try {
+    const user = await user.findOne({ email }).select("+password")
+
+    if(!user) {
+      res.status(404).json({ success: false, error: "Invalid credentials"})
+    }
+
+    const isMatch = await user.matchPassword(password)
+
+    if(!isMatch) {
+      res.status(404).json({success: false, error: "Invalid credentials"})
+    }
+
+    res.status(200).json({
+      success: true,
+      token: "t4rthj4398rt43y8"
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message})
+  }
+};
 
 exports.forgetpassword = (req, res, next) => {
   res.send("Forgot Password Route");
