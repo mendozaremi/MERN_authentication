9017dfca28ef5e6539fc95a0f0ebe787a4a7ecc0
mendozaremi <mendozaremi@gmail.com>
2021-11-11 10:36:22 -0500

    connected to MongoDb server and routes working

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "please provide a valid email"
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minLength: 6,
+    select: false
+  },
+  resetpasswordToken: String,
+  resetPasswordExpire: Date
+});
+
+// middleware to check if password modified
+UserSchema.pre("save", async function(next) {
+  if(!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt)
+  next();
+})
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = UserSchema;
---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+       register,
+       login,
+       forgetpassword,
+       resetpassword 
+      } = require('../controllers/auth');
+
+router.route("/register").post(register)
+
+router.route("/login").post(login)
+
+router.route("/forgotpassword").post(forgetpassword)
+
+router.route("/resetpassword/:resetToken").put(resetpassword)
+
+module.exports = router;
